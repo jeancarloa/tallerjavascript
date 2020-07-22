6ff8f2c8f607f15777d1fde9b9aa87a4fec8aa63
jeancarlo aliendres <jeeanacrlos@gmail.com>
2020-07-22 09:27:15 -0500

    Este es el punto 1 del taller

---
+++ taller1.js
@@ -0,0 +1,24 @@
+const input = document. getElementById("input");
+const table = document. getElementById("table");
+const boton = document. getElementById("boton");
+
+
+
+function agregarTarea () {
+function agregarTarea
+    const fila = document.createElement("tr");
+    
+    
+    file.innerHTML = `
+    <td> $ (input.value)</td>
+    <td>X</td>
+    `;
+     table.appendChild(fila);  
+     
+}
+
+boton.addEventListener("click", agregarTarea);
+boton2.removeEventListener("click", eliminarTarea);
+
+
+
